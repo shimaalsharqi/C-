e8c3ac1ee5631f482653c145f1ef98fd10b8ad2d
Shima Al Sharqi <Shimaalsharqi@gmail.com>
2026-04-16 15:06:24 +0400

    task about string

--- solution06/project01/Program.cs
+++ solution06/project01/Program.cs
@@ -4,44 +4,91 @@
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 5; i++)
+            //for (int i = 0; i < 5; i++)
+            //{
+            //       for (int i = 0; i < 5; i++)
+            // {
+            //     Console.WriteLine("please enter number");
+            //      int num = Convert.ToInt32(Console.ReadLine());
+            //      Console.WriteLine($"the number is {num}");
+
+            //  }
+            //   for (int i = 1; i < 6; i++){
+            //          for (int x = 1; x < 5; x++){
+            //              Console.WriteLine($"({i},{x})");
+            //          }
+            //          }  
+            //Console.WriteLine("please enter number");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //bool stop = true;
+            //for (int n = 2; n < num1 - 1; n++)
+            //{
+            //    if (num1 % n == 0)
+            //    {
+            //        stop = false;
+
+            //    }
+            //}
+
+            //if (stop)
+            //{
+            //    Console.WriteLine("The number is primary");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The number is not primary");
+
+
+
+            //}
+            #region While
+            //int count = 1;
+
+            //Console.WriteLine($"please enter number this is try number {count}");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //Random random = new Random();
+            //int myNumber = random.Next(1, 11);
+
+            //while (num2 != myNumber)
+            //{
+            //   count++;
+            //    if (count <= 3)
+            //    {
+            //        Console.WriteLine($"Incorrect  number this is try number {count} ");
+            //        Console.WriteLine("So please enter again number");
+            //        num2 = Convert.ToInt32(Console.ReadLine());
+
+            //    }
+            //    else {
+            //        break;
+            //    }
+
+            //}
+            #endregion
+            #region Task String
+            Console.WriteLine("please enter any word");
+            string word = Console.ReadLine();
+            string y = "";
+            for (int w = word.Length; w <= 0; w--)
             {
-                //       for (int i = 0; i < 5; i++)
-                // {
-                //     Console.WriteLine("please enter number");
-                //      int num = Convert.ToInt32(Console.ReadLine());
-                //      Console.WriteLine($"the number is {num}");
+                y += word[w];
 
-                //  }
-                //   for (int i = 1; i < 6; i++){
-                //          for (int x = 1; x < 5; x++){
-                //              Console.WriteLine($"({i},{x})");
-                //          }
-                //          }  
-                Console.WriteLine("please enter number");
-                int num1 = Convert.ToInt32(Console.ReadLine());
-                bool stop = true;
-                for (int n = 2; n < num1 - 1; n++)
-                {
-                    if (num1 % n == 0)
-                    {
-                        stop = false;
-
-                    }
-                }
-                if (stop)
-                {
-                    Console.WriteLine("The number is primary");
-
-                }
-                else
-                {
-                    Console.WriteLine("The number is not primary");
-
-
-
-                }
             }
+            if(y == word)
+            {
+                Console.WriteLine("the word is perfact");
+            }
+            else
+            {
+                Console.WriteLine("the word is not perfact");
+            }
+            #endregion
+
+
+
+
+
         }
     }
 }
--- solution06/project01/obj/Debug/net8.0/project01.AssemblyInfo.cs
+++ solution06/project01/obj/Debug/net8.0/project01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e107d979bab85a797b61b6a75535d124441b5989")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0fdb668472d95a6ccba0f50dad4026a99707962")]
 [assembly: System.Reflection.AssemblyProductAttribute("project01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
