13a32229a112838c4936efb74e4d5f85e6b7348a
Shima Al Sharqi <Shimaalsharqi@gmail.com>
2026-05-05 13:57:07 +0400

    Eample about get and set

---
+++ solution12/project01/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace project01
+{
+    internal class Program
+    {
+        static int Calc(int x, int y)
+        {
+            return x + y;
+        }
+
+        static double Calc(double x, double y)
+        {
+            return x + y;
+        }
+
+
+        //static int Calc(int x, int y, int z, int w)
+        //{
+        //    return x + y + z + w;
+        //}
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine(Calc(2, 6));        
+            Console.WriteLine(Calc(2, 9));     
+            //Console.WriteLine(Calc(1, 6, 3, 5)); 
+        }
+
+    
+      
+
+    
+    }
+}
---
+++ solution13/project01/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace project01
+{
+    internal class Program
+    {
+        class Car
+        {
+            public string color;
+            private int temp;
+            public int speed
+            {
+                get { return temp; }
+                set
+                {
+                    if (value > 0)
+                        temp = value;
+                }
+            }
+        }
+            
+        static void Main(string[] args)
+        {
+            Car myCar=new Car();
+            Console.WriteLine("Enter a number of speed");
+            myCar.speed=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(myCar.speed);
+        }
+    }
+}
